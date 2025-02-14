1cad97749ea305231b84fe44765e74c3f4eddae7
Alan Izaguirre <izaguirrea17@gmail.com>
2025-02-14 18:36:18 -0300

    Agregar archivos de proyecto.

---
+++ WordFinder/WordFinder.cs
@@ -0,0 +1,62 @@
+﻿namespace WordFinder
+{
+    public class WordFinder
+    {
+        private readonly List<string> matrix;
+        private readonly int numRows;
+        private readonly int numCols;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            //The constructor sets global values
+            this.matrix = matrix.ToList();
+            numRows = 64;
+            numCols = 64;
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            //I used Hashset so I don't need to look for duplicated values
+            var wordSet = new HashSet<string>(wordstream); 
+            var wordCount = new Dictionary<string, int>();
+
+            //Count how many times each word appears in the matrix and add the information in the dictionary
+            foreach (var word in wordSet)
+            {
+                wordCount.Add(word, CountWord(word));
+            }
+
+            //Sort the dictionary by value to know which are the top 10 words
+            return wordCount
+                .OrderByDescending(kv => kv.Value)
+                .Take(10)
+                .Select(kv => kv.Key);
+        }
+
+        public int CountWord(string word)
+        {
+            int count=0;
+            //Look for the word horizontally
+            for (int row = 0; row < numRows; row++)
+            {
+                if (matrix[row].Contains(word))
+                    count++;
+            }
+
+            //Look for the word vertically
+            for (int col = 0; col < numCols; col++)
+            {
+                string verticalWord = string.Empty;
+                for (int row = 0; row < numRows; row++)
+                {
+                    verticalWord += matrix[row][col];
+                }
+                if (verticalWord.Contains(word))
+                    count++;
+            }
+            //IMPORTANT: I assumed a word could be only once by row or column, It wasn't specified in the problem
+            return count;
+        }
+
+    }
+}
---
+++ WordFinder/readMe.txt
@@ -0,0 +1,21 @@
+﻿Consideraciones para la resolución del ejercicio:
+
+- Se estableció el valor 64 para el número de columnas y de filas como lo especifica el problema, por eso no se usó numRows = this.matrix.Count por ejemplo.
+- Si ninguna palabra es encontrada, el programa devuelve la lista de palabras con el contador en 0.
+- Usamos HashSet para evitar duplicados sin tener que preocuparnos por revisar las palabras.
+- Usamos dictionary para guardar las palabras y la cantidad de veces que aparecen.
+- Las palabras se buscan solamente de izquierda a derecha y de arriba a abajo.
+- El programa es case sensitive.
+- La cantidad total (CT) que se guarda por cada palabra en el diccionario, es igual a la cantidad de veces que aparece la palabra horizontalmente (CH) mas la cantidad de veces que aparece verticalmente (CV) CT=CH+CV
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+Considerations for solving the exercise:
+
+- The value 64 was set for the number of columns and rows as specified in the problem, so numRows = this.matrix.Count was not used.
+- If no word is found, the program returns the list of words with the counter at 0.
+- I used HashSet to avoid duplicates without having to worry about checking the words.
+- I used dictionary to save the words and the number of times they appear.
+- Words are searched only from left to right and from top to bottom.
+- Software is case sensitive.
+- The total amount (CT) that is saved for each word in the dictionary is equal to the number of times the word appears horizontally (CH) plus the number of times it appears vertically (CV) CT=CH+CV
